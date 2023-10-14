5b9436f6942c5b6af6e8fad83a47127c26ab6a0c
Klova9 <68126997+klova9@users.noreply.github.com>
2023-10-14 19:46:56 +0000

    Oct 14, 2023, 2:46 PM

--- Python-main/src/package_klova9/Practice/Minesweeper/cell.py
+++ Python-main/src/package_klova9/Practice/Minesweeper/cell.py
@@ -27,8 +27,7 @@ class Cell:
             borderwidth=8
         )
         btn.bind('<Button-1>', self.left_click_actions ) # Left Click
-        btn.bind('<Button-3>', self.right_click_actions ) # Right Click
-        btn.pack_propagate(True)
+        btn.bind('<Button-3>', self.right_click_actions ) # Right Click=
         self.cell_btn_object = btn
         
            
