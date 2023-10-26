334c9e6601c82075a4d8fa1b095e8ac5580508dd
Klova9 <68126997+klova9@users.noreply.github.com>
2023-10-26 01:40:27 +0000

    Oct 25, 2023, 8:40 PM

--- Python-main/src/package_klova9/Practice/Converters/batch_cbz_to_pdf.py
+++ Python-main/src/package_klova9/Practice/Converters/batch_cbz_to_pdf.py
@@ -1,8 +1,9 @@
 import os
 from PIL import Image
 directory = 'C:/Users/klova9/Documents/Gate/'
+pdf_path = 'D:\Python\Python-main\src\package_klova9\Practice\Converters'
 for path, subdirs, files in os.walk(directory):
     images = [ Image.open(os.path.join(path, name)) for name in files ]
-    print(len(images))
+    images[subdirs].save(pdf_path, "PDF")
     
             
