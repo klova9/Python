f0afd5e14a87f72356c64ff7f97112fef4db3daa
Klova9 <68126997+klova9@users.noreply.github.com>
2023-10-26 01:07:29 +0000

    Oct 25, 2023, 8:07 PM

--- Python-main/src/package_klova9/Practice/Converters/batch_cbz_to_pdf.py
+++ Python-main/src/package_klova9/Practice/Converters/batch_cbz_to_pdf.py
@@ -2,11 +2,11 @@ import os
 
 directory = 'D:/Python/Python-main/src/package_klova9/Practice/Labs/'
 subdirectories = [os.listdir(directory)]
-for subdirectory in subdirectories:
-    print(f'{subdirectory}')
+
     
 files = []
-for file in os.walk[os.join(directory, subdirectory)]:
-    files.append(file)
+for subdirectory in subdirectories:
+    for file in os.walk(subdirectories):
+        files.append(file)
 
 print(files)
