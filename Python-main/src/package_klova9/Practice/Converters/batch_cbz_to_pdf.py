cb3987cc0ec24385fbb0184a34dd9c68f0605f08
Klova9 <68126997+klova9@users.noreply.github.com>
2023-10-26 23:28:13 +0000

    Oct 26, 2023, 6:28 PM

--- Python-main/src/package_klova9/Practice/Converters/batch_cbz_to_pdf.py
+++ Python-main/src/package_klova9/Practice/Converters/batch_cbz_to_pdf.py
@@ -3,20 +3,20 @@ from  pypdf import PdfMerger
 from PIL import Image
 
 directory = 'C:/Users/klova9/Documents/Gate/Gate - Volume 01/'
-
+subdirecrtory = [x[0] for x in os.walk(directory)]
 merger = PdfMerger()
 i=1
+for subdir in subdirecrtory:
+    for images in os.listdir(subdir):
+        if images.endswith('.jpg'):
+            img_path = os.path.join(directory, images)
+            img_jpg = Image.open(img_path)
+            img_pdf = img_jpg.save(img_path[:-4]+'.pdf')
 
-for images in os.listdir(directory):
-    if images.endswith('.jpg'):
-        img_path = os.path.join(directory, images)
-        img_jpg = Image.open(img_path)
-        img_pdf = img_jpg.save(img_path[:-4]+'.pdf')
+    for pdf in os.listdir(subdir):
+        if pdf.endswith('.pdf'):
+            pdf_path = os.path.join(subdir, pdf)
+            merger.append(pdf_path)
 
-for pdf in os.listdir(directory):
-    if pdf.endswith('.pdf'):
-        pdf_path = os.path.join(directory, pdf)
-        merger.append(pdf_path)
-
-merger.write('C:/Users/klova9/Documents/Gate/Gate - Volume 01/Converted.pdf')
-merger.close()
+    merger.write('C:/Users/klova9/Documents/Gate/Gate - Volume 01/Converted.pdf')
+    merger.close()
