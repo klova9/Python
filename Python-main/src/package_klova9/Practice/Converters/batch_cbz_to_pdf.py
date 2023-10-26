71d6063d92c3a05c21727f2aa178262c6ead89e3
Klova9 <68126997+klova9@users.noreply.github.com>
2023-10-26 22:40:28 +0000

    Oct 26, 2023, 5:40 PM

--- Python-main/src/package_klova9/Practice/Converters/batch_cbz_to_pdf.py
+++ Python-main/src/package_klova9/Practice/Converters/batch_cbz_to_pdf.py
@@ -4,6 +4,8 @@ from PIL import Image
 
 directory = 'C:/Users/klova9/Documents/Gate/'
 pdf_path = 'D:\Python\Python-main\src\package_klova9\Practice\Converters'
-subdirs = [x[0] for x in os.walk(directory)]
+# If directory contains multiple subdirectories
+#subdirs = [x[0] for x in os.walk(directory)]
+
 print(subdirs)
     #with Image.open()
