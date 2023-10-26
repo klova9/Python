ea2317cdf937e4306a7f9dd565179057e2721f64
Klova9 <68126997+klova9@users.noreply.github.com>
2023-10-26 01:35:01 +0000

    Oct 25, 2023, 8:35 PM

--- Python-main/src/package_klova9/Practice/Converters/batch_cbz_to_pdf.py
+++ Python-main/src/package_klova9/Practice/Converters/batch_cbz_to_pdf.py
@@ -1,6 +1,4 @@
 import os
 directory = 'C:/Users/klova9/Documents/Gate/'
-for path, subdirs, files in os.walk(directory):
-    for name in files:
-        if name.endswith(".jpg"):
-            print(f'{path}/{name}')
+for subdirectory in os.listdir(directory):
+    
