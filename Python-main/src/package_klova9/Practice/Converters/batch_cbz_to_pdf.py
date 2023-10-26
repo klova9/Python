7df9bd9f3859951167fe5753d1eaf2043a1e2b7d
Klova9 <68126997+klova9@users.noreply.github.com>
2023-10-26 23:09:08 +0000

    Oct 26, 2023, 6:09 PM

--- Python-main/src/package_klova9/Practice/Converters/batch_cbz_to_pdf.py
+++ Python-main/src/package_klova9/Practice/Converters/batch_cbz_to_pdf.py
@@ -1,4 +1,5 @@
 import os
+
 from PIL import Image
 
 directory = 'C:/Users/klova9/Documents/Gate/Gate - Volume 01/'
@@ -7,6 +8,6 @@ pdf_path = 'C:/Users/klova9/Documents/Gate/Convert/'
 i=1
 for images in os.listdir(directory):
     if images.endswith('.jpg'):
-        img_jpg = Image.open(images)
+        img_jpg = Image.open(os.path.join(directory, images))
         img_jpg.save(pdf_path+str(i)+'.pdf')
         i+=1
