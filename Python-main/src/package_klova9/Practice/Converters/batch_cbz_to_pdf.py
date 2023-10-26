f3dde9b722ff674ed78a49585a6ce8149f9cd90e
Klova9 <68126997+klova9@users.noreply.github.com>
2023-10-26 23:17:16 +0000

    Oct 26, 2023, 6:17 PM

--- Python-main/src/package_klova9/Practice/Converters/batch_cbz_to_pdf.py
+++ Python-main/src/package_klova9/Practice/Converters/batch_cbz_to_pdf.py
@@ -6,12 +6,13 @@ directory = 'C:/Users/klova9/Documents/Gate/Gate - Volume 01/'
 pdf_path = 'C:/Users/klova9/Documents/Gate/Convert/'
 merger = PdfMerger()
 i=1
+pdfs = []
 for images in os.listdir(directory):
     if images.endswith('.jpg'):
         img_path = os.path.join(directory, images)
         img_jpg = Image.open(img_path)
         img_pdf = img_jpg.save(img_path[:-4]+'.pdf')
-        merger.append(img_pdf)
+        pdfs.append(img_pdf)
         
 merger.write('C:/Users/klova9/Documents/Gate/Gate - Volume 01/Converted.pdf')
 merger.close()
