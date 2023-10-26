cb131e676c670d1979d4d01e522e9e1f791959a9
Klova9 <68126997+klova9@users.noreply.github.com>
2023-10-26 01:01:09 +0000

    Oct 25, 2023, 8:01 PM

--- Python-main/src/package_klova9/Practice/Converters/batch_cbz_to_pdf.py
+++ Python-main/src/package_klova9/Practice/Converters/batch_cbz_to_pdf.py
@@ -1,6 +1,6 @@
 import os
 
 directory = 'D:/Python/Python-main/src/package_klova9/Practice/Labs/'
-subdirectories = [x[0] for x in os.walk(directory)]
+subdirectories = [os.listdir(directory)]
 for subdirectory in subdirectories:
     print(f'{subdirectory}')
