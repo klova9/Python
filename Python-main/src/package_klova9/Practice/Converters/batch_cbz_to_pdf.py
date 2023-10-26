8ffc44170e48257a389e547809c9ef738a6d3dfe
Klova9 <68126997+klova9@users.noreply.github.com>
2023-10-26 01:19:10 +0000

    Oct 25, 2023, 8:19 PM

--- Python-main/src/package_klova9/Practice/Converters/batch_cbz_to_pdf.py
+++ Python-main/src/package_klova9/Practice/Converters/batch_cbz_to_pdf.py
@@ -1,6 +1,6 @@
 import os
 directory = 'D:/Python/Python-main/src/package_klova9/Practice/Labs/'
-for path, subdirs, files in os.walk(directory):
+for files in os.walk(directory):
     for name in files:
-        if name.endswith(".log"):
-            print(os.path.join(path, name))
+        
+        print(name)
