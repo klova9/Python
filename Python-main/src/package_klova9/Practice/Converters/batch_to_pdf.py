ac7a30be44f5664e8cc21a15778891864dbe2c34
Klova9 <68126997+klova9@users.noreply.github.com>
2023-10-27 22:51:45 +0000

    Oct 27, 2023, 5:51 PM

--- Python-main/src/package_klova9/Practice/Converters/batch_to_pdf.py
+++ Python-main/src/package_klova9/Practice/Converters/batch_to_pdf.py
@@ -2,13 +2,13 @@ import os
 from  pypdf import PdfMerger
 from PIL import Image
 
-directory = r'D:\Downloads\Highschool of the Dead - Full Color Edition [Yen Press]'
+directory = 'D:/Downloads/Highschool of the Dead - Full Color Edition [Yen Press]'
 subdirectorys = [x for x in os.listdir(directory)]
 
 merger = PdfMerger()
 i=1
 for x in subdirectorys:
-    directory = f'{directory}/{x}'
+    directory = os.path.join(directory, x)
     for images in os.listdir(directory):
         if images.endswith('.jpg'):
             img_path = os.path.join(directory, images)
