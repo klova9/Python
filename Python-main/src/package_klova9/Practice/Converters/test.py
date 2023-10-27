23386cabfdd651f52b82e0b941841ababc0e0449
Klova9 <68126997+klova9@users.noreply.github.com>
2023-10-27 22:44:54 +0000

    Oct 27, 2023, 5:44 PM

--- Python-main/src/package_klova9/Practice/Converters/test.py
+++ Python-main/src/package_klova9/Practice/Converters/test.py
@@ -4,6 +4,4 @@ from PIL import Image
 
 directory = r'D:\Downloads\Highschool of the Dead - Full Color Edition [Yen Press]'
 subdirectory = [x for x in os.listdir(directory)]
-for i in subdirectory:
-    directory = os.path.join(directory, i)
-    print(directory + '\n')
+[print(x) for x in subdirectory]
