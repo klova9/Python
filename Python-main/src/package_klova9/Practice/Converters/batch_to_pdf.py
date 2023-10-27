4ad922b59ac7ff0e7860ab080149a6119adbeffe
Klova9 <68126997+klova9@users.noreply.github.com>
2023-10-27 08:22:44 +0000

    Oct 27, 2023, 3:22 AM

--- Python-main/src/package_klova9/Practice/Converters/batch_to_pdf.py
+++ Python-main/src/package_klova9/Practice/Converters/batch_to_pdf.py
@@ -2,7 +2,7 @@ import os
 from  pypdf import PdfMerger
 from PIL import Image
 
-directory = r'D:\Downloads\Highschool of the Dead - Full Color Edition [Yen Press]\Highschool of the Dead - Full Color Edition v01 [Uasaha] (Yen Press)'
+directory = r'D:\Downloads\Highschool of the Dead - Full Color Edition [Yen Press]\Highschool of the Dead - Full Color Edition v02 [Uasaha] (Yen Press)'
 
 merger = PdfMerger()
 i=1
