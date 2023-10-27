f0f1512fd5651e87c74353fffb6ef7f04abe3d56
Klova9 <68126997+klova9@users.noreply.github.com>
2023-10-27 02:59:32 +0000

    Oct 26, 2023, 9:59 PM

--- Python-main/src/package_klova9/Practice/Converters/batch_to_pdf.py
+++ Python-main/src/package_klova9/Practice/Converters/batch_to_pdf.py
@@ -4,19 +4,21 @@ from PIL import Image
 
 directory = r'C:\Users\klova9\Documents'
 
-merger = PdfMerger()
-i=1
 
-for images in os.listdir(directory):
-    if images.endswith('.jpg'):
-        img_path = os.path.join(directory, images)
-        img_jpg = Image.open(img_path)
-        img_pdf = img_jpg.save(img_path[:-4]+'.pdf')
 
-for pdf in os.listdir(directory):
-    if pdf.endswith('.pdf'):
-        pdf_path = os.path.join(directory, pdf)
-        merger.append(pdf_path)
+subfolders = [d for d in os.listdir(directory)]
+for subfolder in subfolders:
+    merger = PdfMerger()
+    for images in os.listdir(directory):
+        if images.endswith('.jpg'):
+            img_path = os.path.join(directory, images)
+            img_jpg = Image.open(img_path)
+            img_pdf = img_jpg.save(img_path[:-4]+'.pdf')
 
-merger.write(os.path.join(directory, 'Converted.pdf'))
-merger.close()
+    for pdf in os.listdir(directory):
+        if pdf.endswith('.pdf'):
+            pdf_path = os.path.join(directory, pdf)
+            merger.append(pdf_path)
+
+    merger.write(os.path.join(directory, 'Converted.pdf'))
+    merger.close()
