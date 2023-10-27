22bc0555e18e413887f7ba066261a380903e0524
Klova9 <68126997+klova9@users.noreply.github.com>
2023-10-27 03:09:13 +0000

    Oct 26, 2023, 10:09 PM

--- Python-main/src/package_klova9/Practice/Converters/batch_to_pdf.py
+++ Python-main/src/package_klova9/Practice/Converters/batch_to_pdf.py
@@ -2,25 +2,21 @@ import os
 from  pypdf import PdfMerger
 from PIL import Image
 
-directory = r'C:\Users\klova9\Documents'
-subdirectories = [d for d in os.listdir(directory)]
+directory = r'D:\Downloads\[Pajeet] Battle Royale\Battle Royale, v15 (2005) [TokyoPop] [Manga-Sketchbook]'
 
+merger = PdfMerger()
 i=1
-def pdf_to_jpg(subdirectory):
-    merger = PdfMerger()
-    for images in os.listdir(directory):
-        if images.endswith('.jpg'):
-            img_path = os.path.join(directory, images)
-            img_jpg = Image.open(img_path)
-            img_pdf = img_jpg.save(img_path[:-4]+'.pdf')
 
-    for pdf in os.listdir(directory):
-        if pdf.endswith('.pdf'):
-            pdf_path = os.path.join(directory, pdf)
-            merger.append(pdf_path)
+for images in os.listdir(directory):
+    if images.endswith('.jpg'):
+        img_path = os.path.join(directory, images)
+        img_jpg = Image.open(img_path)
+        img_pdf = img_jpg.save(img_path[:-4]+'.pdf')
 
-    merger.write(os.path.join(directory, 'Converted.pdf'))
-    merger.close()
-    
-for subdirectory in subdirectories:
-    pdf_to_jpg(subdirectory)
+for pdf in os.listdir(directory):
+    if pdf.endswith('.pdf'):
+        pdf_path = os.path.join(directory, pdf)
+        merger.append(pdf_path)
+
+merger.write(os.path.join(directory, 'Converted.pdf'))
+merger.close()
