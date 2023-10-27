dd086920bd0414dd010361b70ce6291103816365
Klova9 <68126997+klova9@users.noreply.github.com>
2023-10-27 23:13:16 +0000

    Oct 27, 2023, 6:13 PM

--- Python-main/src/package_klova9/Practice/Converters/batch_to_pdf.py
+++ Python-main/src/package_klova9/Practice/Converters/batch_to_pdf.py
@@ -2,24 +2,21 @@ import os
 from  pypdf import PdfMerger
 from PIL import Image
 
-root_directory = 'D:/Downloads/Highschool of the Dead - Full Color Edition [Yen Press]'
-subdirectorys = [x for x in os.listdir(root_directory)]
-
+directory = 'D:\Downloads\Highschool of the Dead - Full Color Edition [Yen Press]\Highschool of the Dead - Full Color Edition v04 [Uasaha] (Yen Press)'
 merger = PdfMerger()
 
-for x in subdirectorys:
-    directory = os.path.join(root_directory, x)
-    for images in os.listdir(directory):
-        if images.endswith('.jpg'):
+for images in os.listdir(directory):
+    if images.endswith('.jpg'):
             img_path = os.path.join(directory, images)
             img_jpg = Image.open(img_path)
             img_pdf = img_jpg.save(img_path[:-4]+'.pdf')
     else:
         pass
 
-    for pdf in os.listdir(directory):
-        if pdf.endswith('.pdf'):
-            pdf_path = os.path.join(directory, pdf)
-            merger.append(pdf_path)
+for pdf in os.listdir(directory):
+    if pdf.endswith('.pdf'):
+        pdf_path = os.path.join(directory, pdf)
+        merger.append(pdf_path)
+        
 merger.write(os.path.join(directory, 'Converted.pdf'))
 merger.close()
