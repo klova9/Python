749dac77a9d3cc31d15169cb30de4ea890775862
Klova9 <68126997+klova9@users.noreply.github.com>
2023-10-27 22:59:29 +0000

    Oct 27, 2023, 5:59 PM

--- Python-main/src/package_klova9/Practice/Converters/test.py
+++ Python-main/src/package_klova9/Practice/Converters/test.py
@@ -2,5 +2,6 @@ import os
 
 root_directory = 'D:/Downloads/Highschool of the Dead - Full Color Edition [Yen Press]'
 for dir, subdir, files in os.walk(root_directory):
-    if files != 'Converted.pdf':
-        os.remove(os.path.join(dir, subdir, files))
+    for file in files:
+        if file != 'Converted.pdf':
+            os.remove(os.path.join(dir, subdir, file))
