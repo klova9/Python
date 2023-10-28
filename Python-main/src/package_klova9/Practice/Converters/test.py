4b1c0b537b0506ff9cbe48d5c39302782e1d0cfe
Klova9 <68126997+klova9@users.noreply.github.com>
2023-10-28 21:07:49 +0000

    Oct 28, 2023, 4:07 PM

--- Python-main/src/package_klova9/Practice/Converters/test.py
+++ Python-main/src/package_klova9/Practice/Converters/test.py
@@ -2,5 +2,5 @@ import os
 
 directory = 'D:\Downloads\Gate'
 for subdirs, dirs, files in os.walk(directory):
-    if (files, dirs) != 'Converted.pdf':
+    if files or dirs != 'Converted.pdf':
         print(files, dirs)
