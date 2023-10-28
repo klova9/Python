bac30c7cc5e5ebcc9b51ada84d5193add598cd72
Klova9 <68126997+klova9@users.noreply.github.com>
2023-10-28 23:21:22 +0000

    Oct 28, 2023, 6:21 PM

--- Python-main/src/package_klova9/Practice/Converters/test.py
+++ Python-main/src/package_klova9/Practice/Converters/test.py
@@ -3,13 +3,13 @@ import send2trash
 
 directory = 'D:\Downloads\Gate'
 subdirs = os.listdir(directory)
-print(subdirs)
-res = []
+
 for (subdirs, dirs, files) in os.walk(directory):
     if (dirs, files) != 'Converted.pdf':
         for file in files:
-            send2trash.send2trash(os.path.join(subdirs, file))
-            print
+            path = os.path.join(subdirs, file)
+            send2trash.send2trash(path)
+            print(f'{path} deleted')
     
 #for subdirs, dirs, files in os.walk(directory):
 """if files or subdirs != 'Converted.pdf':
