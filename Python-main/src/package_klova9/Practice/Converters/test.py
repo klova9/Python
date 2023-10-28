7dbe739675ae8be39afda9c24efbfabcfc087a7c
Klova9 <68126997+klova9@users.noreply.github.com>
2023-10-28 19:59:49 +0000

    Oct 28, 2023, 2:59 PM

--- Python-main/src/package_klova9/Practice/Converters/test.py
+++ Python-main/src/package_klova9/Practice/Converters/test.py
@@ -1,13 +1,4 @@
 import os
-import shutil
-
-root_directory = 'D:/Downloads/Highschool of the Dead - Full Color Edition [Yen Press]'
-subdirectorys = [x for x in os.listdir(root_directory)]
-
-for x in subdirectorys:
-    for file in os.listdir(os.path.join(root_directory, x)):
-        if file != 'Converted.pdf':
-            try:
-                shutil.rmtree(os.path.join(root_directory, x, file))
-            except:
-                pass
+directory = 'D:\Downloads\Highschool of the Dead - Full Color Edition [Yen Press]'
+for subdir, dirs, files in os.walk(directory):
+    print(os.path.join(subdir[dirs], files))
