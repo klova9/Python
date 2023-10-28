e596508c6b080da4cba730a45c05edf2e7e20edc
Klova9 <68126997+klova9@users.noreply.github.com>
2023-10-28 19:56:20 +0000

    Oct 28, 2023, 2:56 PM

--- Python-main/src/package_klova9/Practice/Converters/batch_to_pdf.py
+++ Python-main/src/package_klova9/Practice/Converters/batch_to_pdf.py
@@ -4,19 +4,19 @@ from PIL import Image
 
 directory = 'D:\Downloads\Highschool of the Dead - Full Color Edition [Yen Press]\Highschool of the Dead - Full Color Edition v07 [Uasaha] (Yen Press)'
 merger = PdfMerger()
+for subdir, dirs, files in os.walk(directory):
+    for images in os.listdir(subdir):
+        if images.endswith('.jpg'):
+                img_path = os.path.join(directory, subdir, images)
+                img_jpg = Image.open(img_path)
+                img_pdf = img_jpg.save(img_path[:-4]+'.pdf')
+        else:
+            pass
 
-for images in os.listdir(directory):
-    if images.endswith('.jpg'):
-            img_path = os.path.join(directory, images)
-            img_jpg = Image.open(img_path)
-            img_pdf = img_jpg.save(img_path[:-4]+'.pdf')
-    else:
-        pass
-
-for pdf in os.listdir(directory):
-    if pdf.endswith('.pdf'):
-        pdf_path = os.path.join(directory, pdf)
-        merger.append(pdf_path)
-        
+    for pdf in os.listdir(subdir):
+        if pdf.endswith('.pdf'):
+            pdf_path = os.path.join(directory, subdir, pdf)
+            merger.append(pdf_path)
+            
 merger.write(os.path.join(directory, 'Converted.pdf'))
 merger.close()
