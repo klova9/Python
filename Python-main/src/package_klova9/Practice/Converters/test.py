9636ae12298996aaf81d92ad83b19500ba1c85ba
Klova9 <68126997+klova9@users.noreply.github.com>
2023-10-28 20:01:14 +0000

    Oct 28, 2023, 3:01 PM

--- Python-main/src/package_klova9/Practice/Converters/test.py
+++ Python-main/src/package_klova9/Practice/Converters/test.py
@@ -1,4 +1,4 @@
 import os
 directory = 'D:\Downloads\Highschool of the Dead - Full Color Edition [Yen Press]'
 for subdir, dirs, files in os.walk(directory):
-    print(os.path.join(subdir[dirs], files))
+    print(files)
