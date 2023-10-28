7e54549b88e5d554900a9c882bad3f1fce7c3393
Klova9 <68126997+klova9@users.noreply.github.com>
2023-10-28 21:06:24 +0000

    Oct 28, 2023, 4:06 PM

--- Python-main/src/package_klova9/Practice/Converters/test.py
+++ Python-main/src/package_klova9/Practice/Converters/test.py
@@ -1,7 +1,6 @@
 import os
-directory = 'D:\Downloads\Highschool of the Dead - Full Color Edition [Yen Press]'
-subdir = [x for x in os.listdir(directory) ]
+
+directory = 'D:\Downloads\Gate'
 for subdirs, dirs, files in os.walk(directory):
-    for dir  in dirs:
-        if dir != 'Single pages':
-            print(os.path.join(directory, dir))
+    if (files, dirs) != 'Converted.pdf':
+        print(files, dirs)
