b94f6b86d29928ba60b8d2bb371ab9589dffb23a
Klova9 <68126997+klova9@users.noreply.github.com>
2023-10-28 20:40:58 +0000

    Oct 28, 2023, 3:40 PM

--- Python-main/src/package_klova9/Practice/Converters/test.py
+++ Python-main/src/package_klova9/Practice/Converters/test.py
@@ -2,4 +2,5 @@ import os
 directory = 'D:\Downloads\Highschool of the Dead - Full Color Edition [Yen Press]'
 subdir = [x for x in os.listdir(directory) ]
 for subdirs, dirs, files in os.walk(directory):
-    print(os.path.join(subdir[subdirs], files))
+    for dir  in dirs:
+        print(os.path.join(directory, dir))
