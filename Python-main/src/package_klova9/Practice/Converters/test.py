562f7eb813b500f5b407d50c887aa3681a5a2451
Klova9 <68126997+klova9@users.noreply.github.com>
2023-10-28 21:12:14 +0000

    Oct 28, 2023, 4:12 PM

--- Python-main/src/package_klova9/Practice/Converters/test.py
+++ Python-main/src/package_klova9/Practice/Converters/test.py
@@ -3,4 +3,4 @@ import os
 directory = 'D:\Downloads\Gate'
 for subdirs, dirs, files in os.walk(directory):
     if files or dirs != 'Converted.pdf':
-        os.remove(os.path.join(directory, dirs, files))
+        os.remove((files))
