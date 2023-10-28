d8e8c8e06e7df98fe7e4435f47b6efdf1194c872
Klova9 <68126997+klova9@users.noreply.github.com>
2023-10-28 22:58:37 +0000

    Oct 28, 2023, 5:58 PM

--- Python-main/src/package_klova9/Practice/Converters/test.py
+++ Python-main/src/package_klova9/Practice/Converters/test.py
@@ -2,8 +2,11 @@ import os
 
 directory = 'D:\Downloads\Gate'
 subdirs= os.listdir(directory)
-files = os.listdir(subdirs) 
+files = []
+for x in subdirs:
+    files.append(x)
+print(files)
 #for subdirs, dirs, files in os.walk(directory):
-if files or subdirs != 'Converted.pdf':
+"""if files or subdirs != 'Converted.pdf':
     for file in files:
-        os.remove(os.path.join(file))
+        os.remove(os.path.join(file))"""
