1b8afc14c2a8f11f5b21c65fbfaf7c0a54c4e279
Klova9 <68126997+klova9@users.noreply.github.com>
2023-10-28 20:52:45 +0000

    Oct 28, 2023, 3:52 PM

--- Python-main/src/package_klova9/Practice/Converters/batch_to_pdf.py
+++ Python-main/src/package_klova9/Practice/Converters/batch_to_pdf.py
@@ -7,14 +7,14 @@ merger = PdfMerger()
 for subdirs, dirs, files in os.walk(directory):
     for dir  in dirs:
         if dir != 'Single pages':
-            for images in os.listdir(dir):
+            for images in os.listdir(os.path.join(directory, dir)):
                 if images.endswith('.jpg'):
                         img_path = os.path.join(directory, dir, images)
                         img_jpg = Image.open(img_path)
                         img_pdf = img_jpg.save(img_path[:-4]+'.pdf')
                 else:
                     pass
-            for pdf in os.listdir(dir):
+            for pdf in os.listdir(os.path.join(directory, dir)):
                 if pdf.endswith('.pdf'):
                     pdf_path = os.path.join(directory, dir, pdf)
                     merger.append(pdf_path)
