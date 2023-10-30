04c23a9d6e7b51d49aad3d68c281afc426afd59e
Klova9 <68126997+klova9@users.noreply.github.com>
2023-10-30 20:46:38 +0000

    Oct 30, 2023, 3:46 PM

--- Python-main/src/package_klova9/Practice/Converters/Batch to PDF/run.py
+++ Python-main/src/package_klova9/Practice/Converters/Batch to PDF/run.py
@@ -1,7 +1,7 @@
 import extract
 import batch_to_pdf
 import clean_directory
-directory = 'D:\Downloads\[Pajeet] Battle Royale'
+directory = r'C:\Users\klova9\Documents\Highschool of the Dead - Full Color Edition [Yen Press]'
 extract.extract(directory)
 batch_to_pdf.convert(directory)
 clean_directory.clean_directory(directory)
