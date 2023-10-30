6c3fad12a1c65f702b1dd31790ddb209a761d285
Klova9 <68126997+klova9@users.noreply.github.com>
2023-10-30 20:33:07 +0000

    Oct 30, 2023, 3:33 PM

--- Python-main/src/package_klova9/Practice/Converters/Batch to PDF/extract.py
+++ Python-main/src/package_klova9/Practice/Converters/Batch to PDF/extract.py
@@ -4,10 +4,10 @@ from batch_to_pdf import directory
 
 directory = directory
 subdirs = [x for x in os.listdir(directory)]
-
-for (subdirs, dirs, files) in os.walk(directory):
-    for file in files:
-        if file.endswith('.cbz'):
-            path = os.path.join(subdirs, file)
-            extracted = zipfile.ZipFile(path).extractall(directory)
-            print(f'{path} extracted')
+def extract(directory, subdirs):
+    for (subdirs, dirs, files) in os.walk(directory):
+        for file in files:
+            if file.endswith('.cbz'):
+                path = os.path.join(subdirs, file)
+                extracted = zipfile.ZipFile(path).extractall(directory)
+                print(f'{path} extracted')
