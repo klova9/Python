68be60598d5960c48f1c18383a4e4968845f062c
Klova9 <68126997+klova9@users.noreply.github.com>
2023-10-31 01:44:09 +0000

    Oct 30, 2023, 8:44 PM

--- Python-main/src/package_klova9/Practice/Converters/Batch to PDF/run.py
+++ Python-main/src/package_klova9/Practice/Converters/Batch to PDF/run.py
@@ -1,7 +1,7 @@
 import extract
 import batch_to_pdf
 import clean_directory
-directory = r'C:\Users\klova9\Documents\Highschool of the Dead - Full Color Edition [Yen Press]'
+directory = ''
 extract.extract(directory)
 batch_to_pdf.convert(directory)
 clean_directory.clean_directory(directory)
