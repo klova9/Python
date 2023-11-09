83758f1facc9522169eca2a6fd1fb1c32249bc50
Klova9 <68126997+klova9@users.noreply.github.com>
2023-11-09 00:57:02 +0000

    Nov 8, 2023, 6:57 PM

--- Python-main/src/package_klova9/Practice/Converters/Batch to PDF/clean_directory.py
+++ Python-main/src/package_klova9/Practice/Converters/Batch to PDF/clean_directory.py
@@ -1,7 +1,7 @@
 import os
 import send2trash
 
-def clean_directory(directory):
+def clean_directory(directory) -> None:
     subdirs = os.listdir(directory)
     for (subdirs, dirs, files) in os.walk(directory):
             for file in files:
