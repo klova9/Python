729c6f363714bbd24442bd5934149282b4cb6207
Klova9 <68126997+klova9@users.noreply.github.com>
2023-11-21 16:46:10 +0000

    Nov 21, 2023, 10:46 AM

--- Python-main/src/package_klova9/Practice/Quiz Generator/generate.py
+++ Python-main/src/package_klova9/Practice/Quiz Generator/generate.py
@@ -3,8 +3,8 @@ import time
 import random
 
 quiz = json.load(open('Python-main\src\package_klova9\Practice\Quiz Generator\questions.json'))
-
-for x in range(2):
+n = 3
+for x in range(n):
     i = random.randint(0, len(quiz) - 1)
     print(f'Question: {quiz[i]["question"]}\n(1): {quiz[i]["1"]}\n(2): {quiz[i]["2"]}\n(3): {quiz[i]["3"]}\n(4): {quiz[i]["4"]}')
     answer = input('Answer: ')
@@ -13,7 +13,7 @@ for x in range(2):
     else:
         print('Incorrect')
     time.sleep(1)
-    if x != 2:
-        print('Next Question')
-        time.sleep(1)
+for x in range(n-1):
+    print('Next Question')
+    time.sleep(1)
     
