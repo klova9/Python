3433922ffe9fac56536b157127177c0ac5d15517
Klova9 <68126997+klova9@users.noreply.github.com>
2023-11-21 16:35:10 +0000

    Nov 21, 2023, 10:35 AM

--- Python-main/src/package_klova9/Practice/Quiz Generator/generate.py
+++ Python-main/src/package_klova9/Practice/Quiz Generator/generate.py
@@ -1,4 +1,5 @@
 import json
+from os import wait
 import random
 
 quiz = json.load(open('Python-main\src\package_klova9\Practice\Quiz Generator\questions.json'))
@@ -11,3 +12,6 @@ for x in range(2):
         print('Correct')
     else:
         print('Incorrect')
+    wait(2)
+    print('Next Question')
+    wait(2)
