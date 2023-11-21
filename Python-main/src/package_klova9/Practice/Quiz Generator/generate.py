1afc15dd063879bd21fbe5008f9a596f4b061ef3
Klova9 <68126997+klova9@users.noreply.github.com>
2023-11-21 16:31:18 +0000

    Nov 21, 2023, 10:31 AM

--- Python-main/src/package_klova9/Practice/Quiz Generator/generate.py
+++ Python-main/src/package_klova9/Practice/Quiz Generator/generate.py
@@ -4,4 +4,9 @@ import random
 quiz = json.load(open('Python-main\src\package_klova9\Practice\Quiz Generator\questions.json'))
 
 i = random.randint(0, len(quiz) - 1)
-print(f'Question: {quiz[i]["question"]}\n(1): {quiz[i]["1"]}\n(2): {quiz[i]["2"]}\n(3): {quiz[i]["3"]}\n(4): {quiz[i]["4"]}\nAnswer: ...')
+print(f'Question: {quiz[i]["question"]}\n(1): {quiz[i]["1"]}\n(2): {quiz[i]["2"]}\n(3): {quiz[i]["3"]}\n(4): {quiz[i]["4"]}')
+answer = input('Answer: ')
+if answer == quiz[i]['answer']:
+    print('Correct')
+else:
+    print('Incorrect')
