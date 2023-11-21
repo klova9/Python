f745158058483ba64402cf1ee63dfe261f0b998f
Klova9 <68126997+klova9@users.noreply.github.com>
2023-11-21 16:05:38 +0000

    Nov 21, 2023, 10:05 AM

--- Python-main/src/package_klova9/Practice/Quiz Generator/generate.py
+++ Python-main/src/package_klova9/Practice/Quiz Generator/generate.py
@@ -1,7 +1,5 @@
 import json
 import random
 
-
-with open('Python-main\src\package_klova9\Practice\Quiz Generator\questions.json') as f:
-    quiz = json.dump(f)
-print(random.choice(quiz['question']))
+quiz = json.load(open('Python-main\src\package_klova9\Practice\Quiz Generator\questions.json'))
+print(quiz['question'][0])
