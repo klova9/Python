e9d693f85680f2c3dce8ba03210afe07fdc5c023
Klova9 <68126997+klova9@users.noreply.github.com>
2023-11-23 15:55:21 +0000

    Nov 23, 2023, 9:55 AM

--- Python-main/src/package_klova9/Practice/Quiz Generator/run.py
+++ Python-main/src/package_klova9/Practice/Quiz Generator/run.py
@@ -2,8 +2,3 @@ import tkinter
 import generate
 
 question = generate()
-root = tkinter.Tk()
-frame = tkinter.Frame(root)
-frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
-label = tkinter.Label(frame, question = q)
-root.mainloop()
