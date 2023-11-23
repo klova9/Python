eaf6fc697ef4e122c7f1e7f0c70977b2631fe744
Obree Tribal <klova9@gmail.com>
2023-11-23 09:34:53 -0600

    Delete Python-main/Automation directory

--- Python-main/Automation/Table_To_Chart/run_all.py
+++
@@ -1,5 +0,0 @@
-import os
-os.system('Python-main\Automation\Table_To_Chart\pivot_tables.py')
-os.system('Python-main\Automation\add_table.py')
-os.system('Python-main\Automation\Table_To_Chart\write_formula.py')
-os.system('Python-main\Automation\Table_To_Chart\format_workbook.py')
