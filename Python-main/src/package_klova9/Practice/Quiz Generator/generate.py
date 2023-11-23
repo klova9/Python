601e200a5aa8de961de1613d04c90043f21c1962
Klova9 <68126997+klova9@users.noreply.github.com>
2023-11-23 15:43:38 +0000

    Nov 23, 2023, 9:43 AM

--- Python-main/src/package_klova9/Practice/Quiz Generator/generate.py
+++ Python-main/src/package_klova9/Practice/Quiz Generator/generate.py
@@ -5,16 +5,18 @@ import random
 quiz = json.load(open('Python-main\src\package_klova9\Practice\Quiz Generator\questions.json'))
 n = 3
 m = 0
-for x in range(n):
-    i = random.randint(0, len(quiz) - 1)
-    print(f'Question: {quiz[i]["question"]}\n(1): {quiz[i]["1"]}\n(2): {quiz[i]["2"]}\n(3): {quiz[i]["3"]}\n(4): {quiz[i]["4"]}')
-    answer = input('Answer: ')
-    if answer == quiz[i]['answer']:
-        print('Correct')
-        m += 1
-    else:
-        print('Incorrect')
-    time.sleep(1)
-    print('\n')
-print('You got ' + str(m) + ' out of ' + str(n) + ' correct')
+
+def generate(n, m):
+    for x in range(n):
+        i = random.randint(0, len(quiz) - 1)
+        print(f'Question: {quiz[i]["question"]}\n(1): {quiz[i]["1"]}\n(2): {quiz[i]["2"]}\n(3): {quiz[i]["3"]}\n(4): {quiz[i]["4"]}')
+        answer = input('Answer: ')
+        if answer == quiz[i]['answer']:
+            print('Correct')
+            m += 1
+        else:
+            print('Incorrect')
+        time.sleep(1)
+        print('\n')
+
     
