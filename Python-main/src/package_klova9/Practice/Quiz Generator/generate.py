888d48be2b9ef2edc50c3747a2f2a77506a49622
Klova9 <68126997+klova9@users.noreply.github.com>
2023-11-23 15:57:56 +0000

    Nov 23, 2023, 9:57 AM

--- Python-main/src/package_klova9/Practice/Quiz Generator/generate.py
+++ Python-main/src/package_klova9/Practice/Quiz Generator/generate.py
@@ -4,9 +4,11 @@ import random
 import tkinter
 
 quiz = json.load(open('Python-main\src\package_klova9\Practice\Quiz Generator\questions.json'))
+root = tkinter.Tk()
+frame = tkinter.Frame(root)
+frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
 n = 3
 m = 0
-
 for x in range(n):
     i = random.randint(0, len(quiz) - 1)
     question = quiz[i]["question"]
@@ -20,9 +22,6 @@ for x in range(n):
     time.sleep(1)
     print('\n')
 
-    
-root = tkinter.Tk()
-frame = tkinter.Frame(root)
-frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
 label = tkinter.Label(frame, question)
+label.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
 root.mainloop()
