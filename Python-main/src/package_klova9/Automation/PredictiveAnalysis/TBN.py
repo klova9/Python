4a2d62d08b420140aab767452fd9bb40317187b6
Klova9 <68126997+klova9@users.noreply.github.com>
2023-11-24 21:52:53 +0000

    Nov 24, 2023, 3:52 PM

--- Python-main/src/package_klova9/Automation/PredictiveAnalysis/TBN.py
+++ Python-main/src/package_klova9/Automation/PredictiveAnalysis/TBN.py
@@ -3,10 +3,10 @@ from turtle import title
 import pandas as pd
 
 data = pd.read_csv(r'Python-main\src\package_klova9\Automation\PredictiveAnalysis\netflix_titles.csv')
-print(data.head(10))
-data['title']
-data['type'].drop(row='TV Show'.index(), inplace=True, axis=0)
-
+data_frame = pd.DataFrame(data)
+for i in data_frame['type']:
+    if data_frame['type'][i] != 'Movie':
+        data_frame.drop(data_frame['type'][i], inplace=True, axis=0)
         
-print(data['type'])
+print(data_frame)
     
