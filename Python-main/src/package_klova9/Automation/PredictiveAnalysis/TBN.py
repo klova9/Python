6c555748bbb6459324d68a42d97a60005c5acf62
Klova9 <68126997+klova9@users.noreply.github.com>
2023-11-24 20:56:37 +0000

    Nov 24, 2023, 2:56 PM

--- Python-main/src/package_klova9/Automation/PredictiveAnalysis/TBN.py
+++ Python-main/src/package_klova9/Automation/PredictiveAnalysis/TBN.py
@@ -1,3 +1,8 @@
+from turtle import title
 import pandas as pd
 
-data = pd.read_csv
+data = pd.read_csv(r'Python-main\src\package_klova9\Automation\PredictiveAnalysis\netflix_titles.csv')
+print(data.head(10))
+title = data['title']
+print(title.head(10))
+    
