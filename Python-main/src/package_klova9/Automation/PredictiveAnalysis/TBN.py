bbed4bf225fb8540d1729e3fe1570bc169861437
Klova9 <68126997+klova9@users.noreply.github.com>
2023-11-24 21:48:30 +0000

    Nov 24, 2023, 3:48 PM

--- Python-main/src/package_klova9/Automation/PredictiveAnalysis/TBN.py
+++ Python-main/src/package_klova9/Automation/PredictiveAnalysis/TBN.py
@@ -4,11 +4,9 @@ import pandas as pd
 
 data = pd.read_csv(r'Python-main\src\package_klova9\Automation\PredictiveAnalysis\netflix_titles.csv')
 print(data.head(10))
-title = data['title']
-type = data['type']
-for i in range(len(title)):
-    if type[i] == 'TV Show':
-        new =data.drop(index=i, inplace=True, axis=0)
+data['title']
+data['type'].drop(row='TV Show'.index(), inplace=True, axis=0)
+
         
-print(new)
+print(data['type'])
     
