94dc11b8a9075280184346fe1ec02bb99e189d83
Klova9 <68126997+klova9@users.noreply.github.com>
2023-11-24 19:51:05 +0000

    Nov 24, 2023, 1:51 PM

--- Python-main/src/package_klova9/Practice/HangMan/hang_man.py
+++ Python-main/src/package_klova9/Practice/HangMan/hang_man.py
@@ -4,14 +4,14 @@ import re
 
 # WIP
 
-# This code defines a function called choose_word that selects a random word from a JSON file called 'words.json'. The selected word is then formatted using a regular expression to remove any non-word characters. The formatted word is returned as the result of the function.
+
 def choose_word():
     with open('words.json') as f:
         all_words = json.load(f)
         fmt_words = re.search(r'\b\w+\b', all_words[ran.randint(0, len(all_words) - 1)])
         word = str(fmt_words[0])
         return word
-# This code defines a function called computer_guess that generates a random letter. The function creates two lists, common_letters and uncommon_letters, which contain letters repeated a certain number of times. These lists are then concatenated into a new list called letters. Finally, a random letter is chosen from letters and returned as a string.
+
 '''
 def computer_guess(): 
     common_letters = ['e', 't', 'o', 'a', 'i', 'n', 'n', 's'] * 80
@@ -21,7 +21,7 @@ def computer_guess():
     letter_guess = ran.choice(letters) 
     return str(letter_guess[0])
 '''
-# This code defines a function called check_letter() that prompts the user to enter a guess for a word. It then checks if the guess matches any letters in the word, updates a hidden word with the correct guesses, and returns the hidden word.
+
 def check_letter():
     word = choose_word()
     hidden_word = list('_' * len(word))
