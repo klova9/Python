ffa7c071747af4d7bd2398d60e5227464d7bd557
Klova9 <68126997+klova9@users.noreply.github.com>
2023-11-24 22:24:35 +0000

    Nov 24, 2023, 4:24 PM

--- Python-main/src/package_klova9/Automation/PredictiveAnalysis/TBN.py
+++ Python-main/src/package_klova9/Automation/PredictiveAnalysis/TBN.py
@@ -1,13 +1,7 @@
-from progress.bar import Bar
+
 import pandas as pd
 
 data = pd.read_csv(r'Python-main\src\package_klova9\Automation\PredictiveAnalysis\netflix_titles.csv')
 data_frame = pd.DataFrame(data)
-with Bar('Processing...') as bar:
-    for i in data_frame['type']:
-        if i != 'Movie':
-            data_frame.drop(data_frame['type'].index, inplace=True, axis=0)
-        bar.next()
-        
-print(data_frame)
+
     
