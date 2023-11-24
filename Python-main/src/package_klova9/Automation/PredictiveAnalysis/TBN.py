6f96a1f9619ea6ef5aa0996d59b45ac494997d89
Klova9 <68126997+klova9@users.noreply.github.com>
2023-11-24 21:10:48 +0000

    Nov 24, 2023, 3:10 PM

--- Python-main/src/package_klova9/Automation/PredictiveAnalysis/TBN.py
+++ Python-main/src/package_klova9/Automation/PredictiveAnalysis/TBN.py
@@ -1,4 +1,5 @@
 from turtle import title
+
 import pandas as pd
 
 data = pd.read_csv(r'Python-main\src\package_klova9\Automation\PredictiveAnalysis\netflix_titles.csv')
@@ -9,5 +10,5 @@ for i in range(len(title)):
     if type[i] == 'TV Show':
         new =data.drop(index=i, inplace=True, axis=0)
         
-print(new.head(10))
+print(new)
     
