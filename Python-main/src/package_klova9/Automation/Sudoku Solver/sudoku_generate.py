73d500c68e3d3d91ba44bd4d152a9b19058e16f2
Klova9 <68126997+klova9@users.noreply.github.com>
2024-12-19 18:33:18 +0000

    Dec 19, 2024, 12:33 PM

--- Python-main/src/package_klova9/Automation/Sudoku Solver/sudoku_generate.py
+++ Python-main/src/package_klova9/Automation/Sudoku Solver/sudoku_generate.py
@@ -1,3 +1,4 @@
+from random import randint as random
 def generate_sudoku():
     """
     Generates a sudoku puzzle with a unique solution.
@@ -17,3 +18,25 @@ def generate_full_board():
     # Fill the board with a valid sudoku solution
     solve_sudoku(board)
     return board
+
+def remove_numbers(board):
+    """
+    Removes numbers from the board to create a sudoku puzzle.
+    """
+    # Create a list of all the cells in the board
+    cells = [(row, col) for row in range(9) for col in range(9)]
+    # Shuffle the list of cells
+    random.shuffle(cells)
+    # Create a copy of the board
+    puzzle = copy.deepcopy(board)
+    # Remove numbers from the board until there is only one solution
+    for row, col in cells:
+        # Save the current value of the cell
+        value = puzzle[row][col]
+        # Remove the value from the cell
+        puzzle[row][col] = -1
+        # Check if the board has a unique solution
+        if not has_unique_solution(puzzle):
+            # If the board does not have a unique solution, restore the value
+            puzzle[row][col] = value
+    return puzzle
