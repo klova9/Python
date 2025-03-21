9b0b552dec055c240c94d1ec149b505db063f228
Klova9 <68126997+klova9@users.noreply.github.com>
2025-03-21 19:30:46 +0000

    Mar 21, 2025, 2:30 PM

--- Python-main/src/package_klova9/Practice/Int/count_months.py
+++ Python-main/src/package_klova9/Practice/Int/count_months.py
@@ -8,6 +8,7 @@ date2 = input('Enter the second date in the format MM-DD-YYYY: ')
 
 def count_months(date1, date2):
     date1 = datetime.strptime(date1, '%d-%m-%Y')
-    date2 = datetime.strptime(date2, '%Y-%m-%d')
+    date2 = datetime.strptime(date2, '%d-%m-%Y')
     return abs((date1.year - date2.year) * 12 + date1.month - date2.month)
 
+print(f'The number of months between the two dates is: {count_months(date1, date2)}')
